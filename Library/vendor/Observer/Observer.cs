4de760b0254530aa4e9bb5a6797bbed4ee64890d
Michał Matysek <hello@michalmatysek.com>
2020-05-23 16:41:48 +0200

    add publisher interface and observer abstract class with implementation

---
+++ Library/vendor/Observer/Observer.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace Library.vendor.Observer
+{
+    public abstract class Observer : IPublisher, ISubject
+    {
+        private readonly List<IObserver> _subscribers;
+
+        protected Observer()
+        {
+            _subscribers = new List<IObserver>();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            _subscribers.Add(observer);
+        }
+
+        public void UnregisterObserver(IObserver observer)
+        {
+            _subscribers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            _subscribers.ForEach(subscriber => subscriber.Update());
+        }
+
+        public abstract void Publish();
+    }
+}
